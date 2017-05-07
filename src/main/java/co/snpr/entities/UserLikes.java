5a49a4ac0c47769aa3ad9f63ae1bb8f5d91062c7
bharris62 <blakebharris@gmail.com>
2017-05-06 21:41:41 -0500

    changed name of class to keep up with comment voting

---
+++ src/main/java/co/snpr/entities/UserLikes.java
@@ -0,0 +1,71 @@
+package co.snpr.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="userLikes")
+public class UserLikes {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @ManyToOne
+    Comment comment;
+
+    @ManyToOne
+    User userVoter;
+
+    @Column
+    int upvote;
+
+    @Column
+    int downvote;
+
+    public UserLikes() {
+    }
+
+    public UserLikes(Comment comment, User voter) {
+        this.comment = comment;
+        this.userVoter = voter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Comment getComment() {
+        return comment;
+    }
+
+    public void setComment(Comment comment) {
+        this.comment = comment;
+    }
+
+    public User getVoter() {
+        return userVoter;
+    }
+
+    public void setVoter(User voter) {
+        this.userVoter = voter;
+    }
+
+    public int getUpvote() {
+        return upvote;
+    }
+
+    public void setUpvote(int upvote) {
+        this.upvote = upvote;
+    }
+
+    public int getDownvote() {
+        return downvote;
+    }
+
+    public void setDownvote(int downvote) {
+        this.downvote = downvote;
+    }
+}
